77f1eed026c01044348b77385ced550f7e338b98
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-20 22:25:16 -0300

    Add project files.

---
+++ Diferenca/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, c, d, diferenca;
+
+Console.WriteLine("DIGITE 4 VALORES PARA ENCONTRAR: (A * B - C * D).");
+
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+d = int.Parse(Console.ReadLine());
+
+diferenca = (a * b - c * d);
+
+Console.WriteLine("DIFERENCA = "+diferenca);
